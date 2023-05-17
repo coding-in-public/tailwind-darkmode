1596614e53768b62dc733ed747ee868caae46332
Chris Pennington <chris@codinginpublic.dev>
2023-05-16 22:14:55 -0600

    color upgraded

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,11 @@ module.exports = {
     extend: {
       colors: {
         accent: {
-          1: "hsl(288 95.8% 90.6%)",
-          2: "hsl(168 83.8% 78.2%)",
+          1: "hsl(var(--color-accent1) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)",
         },
-        bkg: "hsl(210 40% 98%)",
-        content: "hsl(217 32.6% 17.5%)",
+        bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+        content: "hsl(var(--color-content) / <alpha-value>)",
       },
       animation: {
         "spin-slower": "spin 35s ease infinite",
